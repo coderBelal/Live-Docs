82a4921aa2108b718fbb3f6e2711fa9c1c9aa136
Belal Uddin <139705476+coderBelal@users.noreply.github.com>
2024-08-26 16:57:18 +0600

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -5,7 +5,7 @@ import {setDoc, doc} from "firebase/firestore"
 import { toast } from "react-toastify";
  
  
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
  
 
 function Register() {
